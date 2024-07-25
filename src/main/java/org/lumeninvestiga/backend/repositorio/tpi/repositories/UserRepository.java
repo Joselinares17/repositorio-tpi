259ec8f74a139b40a04e01c91a117d5e14644110
joselinares17 <josedaniellinares135@gmail.com>
2024-07-25 01:50:39 -0500

    Adding new query

--- src/main/java/org/lumeninvestiga/backend/repositorio/tpi/repositories/UserRepository.java
+++ src/main/java/org/lumeninvestiga/backend/repositorio/tpi/repositories/UserRepository.java
@@ -10,7 +10,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    @Query("SELECT u FROM User u JOIN FETCH u.userDetail ud WHERE ud.emailAddress = :email")
+    @Query("SELECT u FROM User u JOIN FETCH u.userDetail ud WHERE ud.emailAddress =: email")
     Optional<User> findByEmailAddress(String email);
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.userDetail.emailAddress = :email")
+    boolean existsByEmailAddress(String email);
     boolean existsByUsername(String username);
 }
